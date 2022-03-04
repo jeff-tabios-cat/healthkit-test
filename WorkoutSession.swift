7c278964cc99abbe8cc01d7958f1775f6a0eb23d
Jeff <85190419+JeffreyTandem@users.noreply.github.com>
2022-03-04 17:46:01 +0800

    Create healthkit test

---
+++ WorkoutSession.swift
@@ -0,0 +1,50 @@
+
+import Foundation
+
+enum WorkoutSessionState {
+  case notStarted
+  case active
+  case finished
+}
+
+class WorkoutSession {
+  
+  private (set) var startDate: Date!
+  private (set) var endDate: Date!
+  
+  var state: WorkoutSessionState = .notStarted
+  
+  func start() {
+    startDate = Date()
+    state = .active
+  }
+  
+  func end() {
+    endDate = Date()
+    state = .finished
+  }
+  
+  func clear() {
+    startDate = nil
+    endDate = nil
+    state = .notStarted
+  }
+  
+  var completeWorkout: PrancerciseWorkout? {
+    
+    get {
+      
+      guard state == .finished,
+        let startDate = startDate,
+        let endDate = endDate else {
+          return nil
+      }
+      
+      return PrancerciseWorkout(start: startDate,
+                                end: endDate)
+      
+    }
+    
+  }
+  
+}
